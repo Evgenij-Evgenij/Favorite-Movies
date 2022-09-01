836019a495bc49c1d133bc01bd840a4b76ba1900
Evgenij <evgenij.voronov@icloud.com>
2022-09-01 12:53:53 +0300

    Initial Commit

---
+++ Favorite Movies/Favorite_MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Favorite_MoviesApp.swift
+//  Favorite Movies
+//
+//  Created by Евгений Воронов on 01.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Favorite_MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
